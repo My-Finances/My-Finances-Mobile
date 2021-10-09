906fe3478e2ab22e5b704fdd01b67759a248d63f
Nityananda Barbosa <nityanandateixeira@great.ufc.br>
2021-10-08 22:11:00 -0300

    create login module

---
+++ lib/app/modules/login/login_module.dart
@@ -0,0 +1,15 @@
+import 'package:my_finance_mobile/app/modules/login/login_Page.dart';
+import 'package:my_finance_mobile/app/modules/login/login_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class LoginModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => LoginStore()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute('/', child: (_, args) => LoginPage()),
+  ];
+}
---
+++ lib/app/modules/login/login_store.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_triple/flutter_triple.dart';
+
+class LoginStore extends NotifierStore<Exception, int> {
+
+  LoginStore() : super(0);
+
+}
